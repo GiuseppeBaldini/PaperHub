3107d22dfe358a062cc515817c58f0cc35dc8ebb
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-08-11 12:05:42 +0800

    Update structure

--- paperhub.py
+++ paperhub.py
@@ -10,17 +10,20 @@ except (ImportError, ModuleNotFoundError):
     print('Pyperclip module not found. Please download it.')
     exit()
 
-# Get DOI or link
+# Get DOI or link:
 # clipboard
 # argument
 # manual input
 
-# Default: get DOI or link from clipboard,
+# By default get DOI / URL from clipboard
+try:
+    paper_id = pyperclip.paste() 
 
 
 # URL
 # Sci-hub automatic redirect
 
-# file
+# file:
+# download locally
 # send via email
 # send to evernote
