2d1a64ef02e4a27eafa6d0a86d80334d0e3ca851
Giuseppe Baldini <giuseppebaldini@live.com>
2019-12-07 21:48:44 +0800

    Download  pdf in chunks

--- retrieve.py
+++ retrieve.py
@@ -4,6 +4,7 @@
 # Sci-hub automatic redirect
 # Retrieve PDF file
 
+from bs4 import BeautifulSoup
 import requests
 import input
 
@@ -12,16 +13,25 @@ import input
 domain_finder = 'https://whereisscihub.now.sh/go'
 download_path = '..\\Tests\\Papers\\paper.pdf'
 
-# get the most recent scihub domain
+# Get the most recent scihub domain
 scihub = requests.get(domain_finder)
 
-# find our paper on scihub
+# Find paper on scihub
 paper = scihub.url + '/' + input.url
+paper_url = requests.get(paper)
 
-# find pdf url
-# location.href
-# pdf = 
+soup_txt = paper_url.text
+soup = BeautifulSoup(soup_txt, 'html.parser')
 
-# download pdf file in download path
-# file = requests.get(pdf)
-# open(download_path, 'wb').write(file.content)
+article = soup.find('iframe')
+attr = article.attrs['src']
+
+# pdf url
+pdf_url = 'https:' + attr.split('#', 1)[0]
+
+# download pdf file
+r = requests.get(pdf_url, stream=True)
+
+with open(download_path, 'wb') as f:
+    for chunk in r.iter_content(1024):
+        f.write(chunk)
