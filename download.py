d1f585e7c0103ef2384b38127ca1c19cf0c66a24
Giuseppe Baldini <giuseppebaldini@live.com>
2019-12-12 16:11:05 +0800

    Add paper title

--- download.py
+++ download.py
@@ -14,11 +14,10 @@ else:
     exit()
 
 # Save pdf file locally
-with open(download_path, 'wb') as f:
-    for chunk in r.iter_content(1024):
-        f.write(chunk)
-
-# Send via email
-
-
-# Send to evernote
+try: 
+    with open(download_path, 'wb') as f:
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+        print('Download successful.')
+except Exception:
+    print('Download not successful. Check the URL/DOI and try again.')
--- retrieve.py
+++ retrieve.py
@@ -1,6 +1,6 @@
 # Retrieve pdf file URL 
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 import input
 
@@ -8,9 +8,6 @@ import input
 
 # Automatically redirect to active scihub domain
 domain_finder = 'https://whereisscihub.now.sh/go'
-
-# Default local download path
-download_path = '..\\Tests\\Papers\\paper.pdf'
 
 # Open active scihub domain
 scihub = requests.get(domain_finder)
@@ -20,7 +17,7 @@ paper_url = requests.get(scihub.url + '/' + input.url)
 
 # Find the pdf file URL inside the page
 soup_txt = paper_url.text
-soup = BeautifulSoup(soup_txt, 'html.parser')
+soup = bs(soup_txt, 'html.parser')
 article = soup.find('iframe') # tag
 
 # Retrieve tag attribute (our pdf ) 
@@ -31,3 +28,9 @@ except AttributeError:
     exit()
 
 pdf_url = 'https:' + attr.split('#', 1)[0]
+
+# We can use part of the DOI as file name
+pdf_title = pdf_url.split('@', 1)[1] # includes .pdf
+
+# Default local download path
+download_path = 'output\\' + pdf_title
